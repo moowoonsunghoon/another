e94d2a8dd1edb6ecb636246cfff5f623d83ed778
moowoonsunghoon <39836297+moowoonsunghoon@users.noreply.github.com>
2018-10-02 10:55:17 +0900

    Add files via upload

--- amilky/js/ui.js
+++ amilky/js/ui.js
@@ -1,3 +1,4 @@
+//gnb
 // 아이스크림 제조량계산
 $(document).ready(function() {
   $(".icecream").on("keyup", function() {
@@ -54,13 +55,14 @@ $(document).ready(function() {
     var sho = Number($(".sho").val());
     var lub = Number($(".lub").val());
     var set = Number($(".set").val());
-    var total = (ori + set) + (suf + set) + (sho + set) + (lub + set);
+    var lat = Number($(".lat").val());
+    var total = (ori + set) + (suf + set) + (sho + set) + (lub + set)+lat;
     var amo = ((total / 2) * 100)
     var cas = ((total / 2) * 40)
     $(function() {
-      $("#made").html("<h2>제조량</h2> <ul class='made'><li>오리지널 제조량<p>" + (ori + set) + " 병 / "+(ori + set)/2+" L</p></li><li> 슈가프리 제조량<p>" + (suf + set) + " 병 / "+(suf + set)/2+" L</p></li><li> 쇼콜라 제조량<p>" + (sho + set) + " 병 / "+(sho + set) / 2 +" L</p></li><li> 루이보스 제조량<p>" + (lub + set) + " 병 / "+(lub + set)/2+" L</p></li><li>총제조량<p>" + total + " 병 / " + total / 2 + " L </p></li></ul>");
+      $("#made").html("<h2>제조량</h2> <ul class='made'><li>오리지널 제조량<p>" + (ori + set) + " 병 / "+(ori + set)/2+" L</p></li><li> 슈가프리 제조량<p>" + (suf + set) + " 병 / "+(suf + set)/2+" L</p></li><li> 쇼콜라 제조량<p>" + (sho + set) + " 병 / "+(sho + set) / 2 +" L</p></li><li> 루이보스 제조량<p>" + (lub + set) + " 병 / "+(lub + set)/2+" L</p></li><li> 라떼 제조량<p>" + lat + " 병 / "+lat/2+" L</p></li><li>총제조량<p>" + total + " 병 / " + total / 2 + " L </p></li></ul>");
       $("#material").html("<h2>재료</h2> <ul class='material'><li>아몬드<p>" + amo + "g</p></li>" + "<li>캐슈넛<p>" + cas + "g</p></li><li>젤란<p>" + (total * 0.25) + "g</p></li><li>로커스트<p>" + (total * 0.125) + "g</p></li></ul><ul class='material'><li>소금<p>" + (total * 0.75) + "g</p></li><li>레시틴<p>" + (total * 0.5) + "g</p></li><li>칼륨<p>" + (total * 0.5) + "g</p></li></ul>");
-      $("#syrup").html("<h2>시럽</h2> <ul class='syrup'><li><h4>오리지널</h4></li><li> 물<p>" + (ori + set) * 50 + "g</p></li><li>설탕<p>" + (ori + set) * 20 + "g</p></li><li> 바닐라빈<p>" + ((ori + set) * 0.1).toFixed(1) + "ea</p></li></ul><ul class='syrup'><li><h4>슈가프리</h4></li><li> 물<p>" + (suf + set) * 50 + "g</p></li><li>에리스리톨<p>" + (suf + set) * 10 + "g</p></li><li>바닐라빈<p>" + ((suf + set) * 0.1).toFixed(1) + "ea</p></li></ul><ul class = 'syrup'><li><h4>쇼콜라</h4></li> <li>물<p>" + (sho + set) * 50 + "g</p></li><li>설탕<p>" + (sho + set) * 30 + "g</p></li><li>카카오분<p>" + (sho + set) * 12.5 + "g</p></li></ul><ul class='syrup'><li><h4>루이보스</h4></li><li>물<p>" + (lub + set) * 50 + "g</p></li><li>설탕<p>" + (lub + set) * 20 + "g</p></li><li>루이보스잎<p>" + (lub + set) * 3.5 + "g</p></li></ul>");
+      $("#syrup").html("<h2>시럽</h2> <h4>오리지널</h4><ul class='syrup'><li> 물<p>" + (ori + set) * 50 + "g</p></li><li>설탕<p>" + (ori + set) * 20 + "g</p></li><li> 바닐라빈<p>" + ((ori + set) * 0.1).toFixed(1) + "ea</p></li></ul><h4>슈가프리</h4><ul class='syrup'><li> 물<p>" + (suf + set) * 50 + "g</p></li><li>에리스리톨<p>" + (suf + set) * 10 + "g</p></li><li>바닐라빈<p>" + ((suf + set) * 0.1).toFixed(1) + "ea</p></li></ul><h4>쇼콜라</h4><ul class = 'syrup'><li>물<p>" + (sho + set) * 50 + "g</p></li><li>설탕<p>" + (sho + set) * 30 + "g</p></li><li>카카오분<p>" + (sho + set) * 12.5 + "g</p></li></ul><h4>루이보스</h4><ul class='syrup'><li>물<p>" + (lub + set) * 50 + "g</p></li><li>설탕<p>" + (lub + set) * 20 + "g</p></li><li>루이보스잎<p>" + (lub + set) * 3.5 + "g</p></li></ul><h4>라떼</h4><ul class='syrup'><li>커피원액<p>" + lat * 50 + "g</p></li><li>설탕<p>" + lat * 20 + "g</p></li></ul>");
     })
     if (amo >= 1000) {
       var amo = amo / 1000;
